5c26198d8632d4d72df1df24994b7e187368666e
JEONG <dogug2865@gmail.cpm>
2020-10-18 20:33:25 +0900

    feat: iex api call

--- djangostock/stock/views.py
+++ djangostock/stock/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
+import requests
+import json
 # Create your views here.
+
+
+def home(request):
+
+    try:
+        ticker = request.GET['ticker']
+        stock_api = requests.get(
+            "https://cloud.iexapis.com/stable/stock/"+ticker+"/quote?token=pk_db6737ecb57f4d10a7eded7efcee08f8")
+        stock = json.loads(stock_api.content)
+    except Exception as e:
+        stock = ""
+
+    content = {'stock': stock}
+    return render(request, 'stock/home.html', content)
